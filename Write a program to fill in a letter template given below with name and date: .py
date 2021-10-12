4a56a54b38fa61cd1ac0bb4fc61b18d0425d1bf7
Sahil Dubey <83882002+sahildubey28@users.noreply.github.com>
2021-10-12 12:16:41 +0530

    Create Write a program to fill in a letter template given below with name and date: .py

---
+++ Write a program to fill in a letter template given below with name and date: .py
@@ -0,0 +1,9 @@
+Name = input("Enter your Name: ")
+Date = input("Enter Date: ")
+print("''' Dear " + Name + "\n You are selected" + "\n" + Date + "'''") 
+
+# Output: Enter your Name: Sahil Dubey
+#         Enter Date: 28-08-2021
+#         ''' Dear Sahil Dubey
+#             You are selected
+#             28-08-2021'''
