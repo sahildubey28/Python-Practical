75f45eb795114f994889f7f896a9a64f6db83c8d
Sahil Dubey <83882002+sahildubey28@users.noreply.github.com>
2021-10-12 12:12:25 +0530

    Create Write a program to create a dictionary of Hindi words with values as their English Translation. Provide user with an option to look it up..py

---
+++ Write a program to create a dictionary of Hindi words with values as their English Translation. Provide user with an option to look it up..py
@@ -0,0 +1,8 @@
+dict = {'Masti' :'Fun', 'Gaana' :'Song', 'Nachna' :'Dance'}
+
+for key in dict:
+    print (key)
+print("Enter the hindi word from above list whose english meaning you want : ")
+x=input()
+
+print(dict[x])
