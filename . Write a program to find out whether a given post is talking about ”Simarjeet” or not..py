5e140dadb04342160b6c03279cfb94982d14ca4c
Sahil Dubey <83882002+sahildubey28@users.noreply.github.com>
2021-10-12 12:20:42 +0530

    Create . Write a program to find out whether a given post is talking about ”Simarjeet” or not..py

---
+++ . Write a program to find out whether a given post is talking about ”Simarjeet” or not..py
@@ -0,0 +1,12 @@
+post = input("Enter a post: ")
+if (("Simarjeet" in post) or ("simarjeet" in post) or ("SIMARJEET" in post)):
+    print("Yes! the post is talking about Simarjeet")
+else: 
+    print("No! the post is not talking about Simarjeet")
+
+
+# Output :    Enter a post: Simarjeet
+#             Yes! the post is talking about Simarjeet
+            
+#             Enter a post: Sahil
+#             No! the post is not talking about Simarjeet
