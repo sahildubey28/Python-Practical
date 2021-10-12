7655c639793a28b423d89dfde097aed55728f78e
Sahil Dubey <83882002+sahildubey28@users.noreply.github.com>
2021-10-12 12:13:50 +0530

    Create Write a program to calculate the factorial of the given number.py

---
+++ Write a program to calculate the factorial of the given number.py
@@ -0,0 +1,10 @@
+num = int(input("Enter positive Number: "))
+fact=1
+while(num>0):
+    fact=fact*num;
+    num-=1
+
+print("Factorial of number = ", fact)
+
+# Output: Enter positive Number: 4
+#         Factorial of number =  24
