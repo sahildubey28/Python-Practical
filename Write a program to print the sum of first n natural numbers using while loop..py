18d62c22f48125219f610ee231fbc4de394ff6bf
Sahil Dubey <83882002+sahildubey28@users.noreply.github.com>
2021-10-12 12:27:48 +0530

    Create Write a program to print the sum of first n natural numbers using while loop..py

---
+++ Write a program to print the sum of first n natural numbers using while loop..py
@@ -0,0 +1,10 @@
+num = int(input("Enter positive Number: "))
+sum = 0
+
+while(num > 0):
+    sum += num
+    num -= 1
+print("The sum is", sum)
+
+# Output: Enter positive Number: 10
+#         The sum is 55
