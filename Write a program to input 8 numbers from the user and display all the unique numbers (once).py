c0b194a0105537c062dedc80475929fefeca22ac
Sahil Dubey <83882002+sahildubey28@users.noreply.github.com>
2021-10-12 12:18:36 +0530

    Create  Write a program to input 8 numbers from the user and display all the unique numbers (once).py

---
+++ Write a program to input 8 numbers from the user and display all the unique numbers (once).py
@@ -0,0 +1,18 @@
+A = []
+print("Enter the Element of List :")
+for i in range(8):
+    A.append(input(""))
+
+print("The unique values from the List is :", set(A))
+
+# Output : Enter the Element of List :
+#             1
+#             2
+#             3
+#             5
+#             2
+#             3 
+#             4
+#             6
+#             The unique values from the List is : {'2', '3', '5', '1', '6', '4'}
+
