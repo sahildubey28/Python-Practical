4d34f0a2c110a0398270e76c2a6dc688ce096bee
Sahil Dubey <83882002+sahildubey28@users.noreply.github.com>
2021-10-12 12:29:18 +0530

    Create Write a program to check whether a username consists of less than 10 characters or not.py

---
+++ Write a program to check whether a username consists of less than 10 characters or not.py
@@ -0,0 +1,12 @@
+usernme = input("Enter your Username: ")
+C = len(usernme)
+if(C<10):
+    print("Username consists of less than 10 characters")
+else:
+    print("Username consists of equal or more than 10 characters")
+
+# Output :    Enter your Username: sahildubey28
+#             Username consists of equal or more than 10 characters
+
+            # Enter your Username: sahildubey
+            # Username consists of less than 10 characters
