4d9aac9748ae8418bb7c8389dd5189e0e7d144b6
Sahil Dubey <83882002+sahildubey28@users.noreply.github.com>
2021-10-12 12:23:43 +0530

    Create Write a program to detect double spaces in a string.py

---
+++ Write a program to detect double spaces in a string.py
@@ -0,0 +1,9 @@
+string = input("Enter String: ")
+res = "  " in string
+print("Does string contain spaces ? " + str(res))
+
+# Output: Enter String: Sahil Dubey                    //with 2 spaces
+#         Does string contain spaces ? True  
+
+#         Enter String: Sahil Dubey                    //with 1 space
+#         Does string contain spaces ? False 
