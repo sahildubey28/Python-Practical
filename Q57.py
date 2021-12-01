899c24b7c43da52d6e4da10d0b59e45a66c6d845
Sahil Dubey <83882002+sahildubey28@users.noreply.github.com>
2021-12-01 19:38:32 +0530

    Create Q57.py

---
+++ Q57.py
@@ -0,0 +1,15 @@
+# Write a Python function to check whether the given integer is a prime number or not.
+
+def test_prime(n):
+    if (n==1):
+        return False
+    elif (n==2):
+        return True;
+    else:
+        for x in range(2,n):
+            if(n % x==0):
+                return False
+        return True             
+print(test_prime(9))
+
+# OUTPUT:-False
