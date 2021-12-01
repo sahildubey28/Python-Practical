0ef76d4d1a1e9e259f1fb3eda4a0fbfbd3bf0f4f
Sahil Dubey <83882002+sahildubey28@users.noreply.github.com>
2021-12-01 19:34:50 +0530

    Create Q49.py

---
+++ Q49.py
@@ -0,0 +1,10 @@
+# Write a function x(n) for computing an element in the sequence xn=n^2+1. Call the function for n=4 and
+# write out the result.
+
+
+def x(n):
+    return n*n + 1
+
+print(x(4))
+
+# OUTPUT:-17
