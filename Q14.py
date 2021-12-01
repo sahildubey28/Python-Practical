5405aed6e491761dc7a6d06e3a89b01aaba219a3
Sahil Dubey <83882002+sahildubey28@users.noreply.github.com>
2021-12-01 19:15:24 +0530

    Create Q14.py

---
+++ Q14.py
@@ -0,0 +1,30 @@
+# Q14. Take input of age of 3 people by user and determine oldest and youngest among them
+
+# Take Ages
+person1 = eval(input("Enter age of person 1 : "))
+person2 = eval(input("Enter age of person 2 : "))
+person3 = eval(input("Enter age of person 3 : "))
+
+# check oldest
+if person1 > person2 and person1 > person3:
+    print("Person 1 is oldest")
+elif person2 > person1 and person2 > person3:
+    print("Person 2 is oldest")
+elif person3 > person1 and person3 > person2:
+    print("Person 3 is oldest")
+
+# check youngest
+if person1 < person2 and person1 < person3:
+    print("Person 1 is youngest")
+elif person2 < person1 and person2 < person3:
+    print("Person 2 is youngest")
+elif person3 < person1 and person3 < person2:
+    print("Person 3 is youngest")
+
+# Output :
+
+# Enter age of person 1 : 30
+# Enter age of person 2 : 20
+# Enter age of person 3 : 50 
+# Person 3 is oldest
+# Person 2 is youngest
