ba7dbe371edb607cfacd4299846ca2544c4d5e4d
Sahil Dubey <83882002+sahildubey28@users.noreply.github.com>
2021-12-01 19:44:40 +0530

    Create Q70.py

---
+++ Q70.py
@@ -0,0 +1,10 @@
+# Python program to find the even numbers in a List.
+
+list= [11,23,45,23,64,22,11,24]
+print( "even no= ")
+for num in list:
+   
+   if num % 2 == 0:
+      print(num)
+
+# OUTPUT:- even no = 64 22 24
