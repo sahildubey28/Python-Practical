630b04d210dab16e9a9dcadc1eb8508989d222b3
Sahil Dubey <83882002+sahildubey28@users.noreply.github.com>
2021-12-01 19:47:13 +0530

    Create Q4.py

---
+++ Q4.py
@@ -0,0 +1,12 @@
+# Q4. Accept two numbers from the user and multiply them together.
+
+n1 = int(input("Enter a number: "))
+n2 = int(input("Enter another number: "))
+
+print(n1*n2)
+
+# Output :
+
+# Enter a number: 10
+# Enter another number: 10
+# 100
