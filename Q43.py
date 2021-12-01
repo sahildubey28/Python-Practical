16732517216e43bf391e1456fae683120276ac2d
Sahil Dubey <83882002+sahildubey28@users.noreply.github.com>
2021-12-01 19:31:43 +0530

    Create Q43.py

---
+++ Q43.py
@@ -0,0 +1,10 @@
+# Write a Python program to remove spaces from a given string.
+
+def remove(string):
+    return string.replace(" ", "")
+      
+# Driver Program
+string = 'd e e p'
+print(remove(string))
+
+# OUTPUT:- deep
