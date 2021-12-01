34b90836203795a9c27e50538d90faf1cf825e2c
Sahil Dubey <83882002+sahildubey28@users.noreply.github.com>
2021-12-01 19:46:48 +0530

    Create Q3.py

---
+++ Q3.py
@@ -0,0 +1,9 @@
+# Q3. Write a Python program to get the volume of a sphere with radius 10.
+
+r = 10
+vol=(4/3)*3.14*r*r*r
+print("volume of a sphere = ",round(vol,4))
+
+# Output:
+
+# volume of a sphere =  4186.6667
