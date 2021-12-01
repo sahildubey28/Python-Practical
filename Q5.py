2c014a924705d121e2823b9dae54a1592230b5c4
Sahil Dubey <83882002+sahildubey28@users.noreply.github.com>
2021-12-01 19:47:35 +0530

    Create Q5.py

---
+++ Q5.py
@@ -0,0 +1,10 @@
+# Q5. Write a Python program that accepts an integer (a) and computes the value of a+aa+aaa.
+
+a = int(input("Enter a number : "))
+n = a+a*a+a*a*a
+print("Value = ",n)
+
+# Output :
+
+# Enter a number : 10
+# Value =  1110
