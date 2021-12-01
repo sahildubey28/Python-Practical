e77aad026cbcf9dc50a4986bc5c58d94c561c3d7
Sahil Dubey <83882002+sahildubey28@users.noreply.github.com>
2021-12-01 19:30:20 +0530

    Create Q40.py

---
+++ Q40.py
@@ -0,0 +1,11 @@
+# Q40. Write a Python program that takes input from the user and displays that input back in upper and lower cases.
+
+user_input = input("Enter a String : ")
+print("String in UpperCase : ", user_input.upper())
+print("String in LowerCase : ", user_input.lower())
+
+# Output:
+
+# Enter a String : English
+# String in UpperCase :  ENGLISH
+# String in LowerCase :  english
