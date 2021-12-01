77806a41436d90b7d739d2b26ed1eddce2fb1db8
Sahil Dubey <83882002+sahildubey28@users.noreply.github.com>
2021-12-01 19:22:17 +0530

    Create Q25.py

---
+++ Q25.py
@@ -0,0 +1,17 @@
+# Q25. Program to find factorial of a given number
+
+num = int(input("Enter a number: "))    
+factorial = 1    
+if num < 0:    
+   print(" Factorial does not exist for negative numbers")    
+elif num == 0:    
+   print("The factorial of 0 is 1")    
+else:    
+   for i in range(1,num + 1):    
+       factorial = factorial*i    
+   print("The factorial of",num,"is",factorial) 
+
+# Output:
+
+# Enter a number: 10
+# The factorial of 10 is 3628800
