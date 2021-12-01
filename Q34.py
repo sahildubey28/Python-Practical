04050d97ec4231c8038c10aa9815a295720b7684
Sahil Dubey <83882002+sahildubey28@users.noreply.github.com>
2021-12-01 19:26:58 +0530

    Create Q34.py

---
+++ Q34.py
@@ -0,0 +1,9 @@
+# Q34. Write a Python program to calculate the length of a given string.
+
+str = input("Enter a string: ")
+print("Length of the input string is:", len(str))
+
+# output:
+
+# Enter a string: Sahil
+# Length of the input string is: 5
