96ae3764b5b8a96095afcb67fe0073827cb56683
Sahil Dubey <83882002+sahildubey28@users.noreply.github.com>
2021-12-01 19:30:43 +0530

    Create Q41.py

---
+++ Q41.py
@@ -0,0 +1,8 @@
+# Write a Python program to count occurrences of a substring in a string.
+
+str1 = 'the quick brown fox jumps over the lazy dog.'
+print()
+print(str1.count("the"))
+print()
+
+# OUTPUT:- 2
