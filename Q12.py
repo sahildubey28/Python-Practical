0bafa928d4718a407db69a7d4da73bb4b1ea04b1
Sahil Dubey <83882002+sahildubey28@users.noreply.github.com>
2021-12-01 19:11:21 +0530

    Create Q12.py

---
+++ Q12.py
@@ -0,0 +1,12 @@
+# Q12 To check whether a given number is even or odd.
+
+num = int(input("Enter a number: "))
+if (num % 2) == 0:
+   print("{0} is Even".format(num))
+else:
+   print("{0} is Odd".format(num))
+
+# Output :
+
+# Enter a number: 43
+# 43 is Odd
