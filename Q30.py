82dfd49d4b4cecee06ec3c6a7ff8c321d394a93d
Sahil Dubey <83882002+sahildubey28@users.noreply.github.com>
2021-12-01 19:24:37 +0530

    Create Q30.py

---
+++ Q30.py
@@ -0,0 +1,19 @@
+# Q30. Print downward Half-Pyramid Pattern with Star (asterisk) 
+'''
+        * * * * * 
+         * * * * 
+          * * * 
+           * * 
+            * 
+'''
+
+rows = 5
+  
+k = 2 * rows - 2  
+for i in range(rows, -1, -1):  
+    for j in range(k, 0, -1):  
+        print(end=" ")  
+    k = k + 1  
+    for j in range(0, i + 1):  
+        print("*", end=" ")  
+    print("") 
