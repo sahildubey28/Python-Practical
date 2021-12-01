dbca13571619a7de2fd7668365adacbefe18e470
Sahil Dubey <83882002+sahildubey28@users.noreply.github.com>
2021-12-01 19:47:56 +0530

    Create Q6.py

---
+++ Q6.py
@@ -0,0 +1,9 @@
+# Q6. Write a Python program to calculate the length of a string
+
+str = input("Enter a string: ")
+print("Length of the input string is:", len(str))
+
+# Output:
+
+# Enter a string: Rohit Jain
+# Length of the input string is: 10
