e1d8a65b120e94814515edfc9429e70c0225d792
Sahil Dubey <83882002+sahildubey28@users.noreply.github.com>
2021-12-01 19:48:17 +0530

    Create Q7.py

---
+++ Q7.py
@@ -0,0 +1,24 @@
+# Q7. Write a Python program to parse a string to Float & Integer
+
+balance_str = "1500"
+balance_int = int(balance_str)
+
+# print the type
+print(type(balance_int))
+# print the value
+print(balance_int)
+
+balance_str = "1500.4"
+balance_float = float(balance_str)
+
+# print the type
+print(type(balance_float))
+# print the value
+print(balance_float)
+
+# output : 
+
+# <class 'int'>
+# 1500
+# <class 'float'>
+# 1500.4
