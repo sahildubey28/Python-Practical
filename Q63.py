b38a5b9c657023192aa554b6432ee4823319ca34
Sahil Dubey <83882002+sahildubey28@users.noreply.github.com>
2021-12-01 19:41:43 +0530

    Create Q63.py

---
+++ Q63.py
@@ -0,0 +1,19 @@
+# Python program to insert a number to given position in a list
+
+def insert_spec_position(x, n_list, pos):
+    return n_list[:pos-1]+[x]+n_list[pos-1:]
+n_list = [1,1,2,3,4,4,5,1]
+print("Original list:") 
+print(n_list)
+kth_position = 3
+x = 12
+result = insert_spec_position(x, n_list, kth_position)
+print("\nAfter inserting an element at kth position in the said list:")
+print(result)
+
+
+# OUTPUT:- Original list:
+#         [1, 1, 2, 3, 4, 4, 5, 1]
+
+# After inserting an element at kth position in the said list:
+#         [1, 1, 12, 2, 3, 4, 4, 5, 1]
